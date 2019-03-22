0d9f72056c6217787b7d9362c6d8709432e23f2a
Lawrence Weinstein <lawrence.e.weinstein@gmail.com>
2019-03-22 13:37:35 -0400

    Replaced missing ages values with mean

--- Titanic.R
+++ Titanic.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(tidyr)
 
 #load file into memory
 titanic_data <- tbl_df(read.csv("titanic_original.csv"))
@@ -6,3 +7,14 @@ titanic_data <- tbl_df(read.csv("titanic_original.csv"))
 #Replace missing/empty strings with "S"
 titanic_data <- titanic_data %>%
   mutate(embarked = replace(embarked, embarked == "", "S"))
+
+#Calculate mean of age
+mean_age = mean(titanic_data$age, na.rm = TRUE)
+
+#replace missing age values with mean age
+#Note:  Would have been possible to use median/mode age
+#Also could choose nonsensical values like 999 to show age missing
+#If we assume that unavailable ages have nothing to do with actual age
+#Using mean age can help us gain additional data for predictive modeling
+titanic_data <- titanic_data %>% 
+  mutate(age = replace(age, is.na(age), mean_age))
